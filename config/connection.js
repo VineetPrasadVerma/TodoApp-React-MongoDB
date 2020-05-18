041c6a777fbb5f481298b955a6f708ca4650e506
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-05-18 23:43:06 +0530

    removed deprecated warning

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const URI = process.env.DATABASE_URL
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
     console.log('MongoDB started on port 27017')
   } catch (err) {
     console.log(err)
