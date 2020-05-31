a5e91b6be1de88f180bb0fe4187f12b0ab595b36
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-06-01 04:38:23 +0530

    Add color for priority

--- client/src/components/SubtaskDetails.js
+++ client/src/components/SubtaskDetails.js
@@ -12,6 +12,16 @@ const SubTaskDetails = ({ task, subtask, handleError }) => {
   const [completed, setCompleted] = useState(subtask.completed)
   const [expandSubtask, setExpandSubtask] = useState(false)
 
+  let color = ''
+
+  if (subtask.priority === 3) {
+    color = 'red'
+  } else if (subtask.priority === 2) {
+    color = 'orange'
+  } else if (subtask.priority === 1) {
+    color = 'green'
+  }
+
   const handleUpdateSubtask = async (event, id, value) => {
     event.preventDefault()
     // console.log({ [event.target.id]: value })
@@ -59,7 +69,7 @@ const SubTaskDetails = ({ task, subtask, handleError }) => {
         }}
       />
       <span className={completed ? 'finished' : ''} id='subtaskName'>{subtask.name}</span>
-      <FontAwesomeIcon className={completed ? 'finished' : ''} id='expandIcon' icon={faArrowCircleDown} onClick={() => setExpandSubtask(!expandSubtask)} />
+      <FontAwesomeIcon style={{ color: color }} className={completed ? 'finished' : ''} id='expandIcon' icon={faArrowCircleDown} onClick={() => setExpandSubtask(!expandSubtask)} />
       <FontAwesomeIcon className={completed ? 'finished' : ''} id='deleteIcon' icon={faTrash} onClick={() => handleDeleteSubtask(subtask._id)} />
       <FontAwesomeIcon className={completed ? 'finished' : ''} id='editIcon' icon={faPencilAlt} onClick={() => setEditInput(true)} />
       {expandSubtask ? (
